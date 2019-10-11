06a369084fa8f12b70f1b770cc5df21f00f33a3b
svarez <ericksuarezdev@gmail.com>
2019-10-11 02:14:03 -0500

    Template basico

--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
+import { NzSelectModule, NzIconModule, NzBreadCrumbModule, NzGridModule, NzListModule, NzButtonModule  } from  'ng-zorro-antd'
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
 
 import { WelcomeComponent } from './welcome.component';
+import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
 
 
 @NgModule({
-  imports: [WelcomeRoutingModule],
+  imports: [
+    WelcomeRoutingModule, 
+    FormsModule, 
+    ChartsModule,
+    NzListModule,
+    NzGridModule, 
+    NzIconModule,
+    NzButtonModule,
+    NzSelectModule, 
+    NzBreadCrumbModule,
+  ],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
 })
