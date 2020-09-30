2f7fdd5439dde0a856e107bd14e67a603b05e630
Erick Suarez <ericksuarezdev@gmail.com>
2020-09-29 20:28:33 -0500

    Add dodo to list

--- ext/src/browser_action/js/script.js
+++ ext/src/browser_action/js/script.js
@@ -1,5 +1,13 @@
-const btnAddDodo = document.getElementById('btnAddDodo').addEventListener("click", addDodo);
+const dodoText = document.getElementById('dodoText')
+      dodoList = document.getElementById('dodoList'),
+      btnAddDodo = document.getElementById('btnAddDodo');
+
+dodoText.addEventListener('keyup', e => { if (event.keyCode === 13) addDodo() })
+btnAddDodo.addEventListener('click', addDodo);
 
 function addDodo() {
-   console.log("...")
+   const item = document.createElement('li')
+   item.innerHTML = dodoText.value
+   dodoList.appendChild(item)
+   dodoText.value = ''   
 }
