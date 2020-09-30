f74928fa3f83dda00ee22233fa02b4700a73c52c
Erick Suárez <42328586+esvarez@users.noreply.github.com>
2020-09-30 00:10:07 -0500

    Mark task as done (#16)
    * Add done and delete button
    
    * Add buton remove

--- ext/src/browser_action/js/script.js
+++ ext/src/browser_action/js/script.js
@@ -1,13 +1,29 @@
 const dodoText = document.getElementById('dodoText')
       dodoList = document.getElementById('dodoList'),
+      btnDodoDone = document.getElementsByClassName('btnDodoDone'),
       btnAddDodo = document.getElementById('btnAddDodo');
 
 dodoText.addEventListener('keyup', e => { if (event.keyCode === 13) addDodo() })
 btnAddDodo.addEventListener('click', addDodo);
 
+for (let i = 0; i < btnDodoDone.length; i++) {
+   btnDodoDone[i].addEventListener('click', markDone);
+}
+
 function addDodo() {
-   const item = document.createElement('li')
-   item.innerHTML = dodoText.value
-   dodoList.appendChild(item)
-   dodoText.value = ''   
+   if (dodoText.value != '') {
+      const item = document.createElement('li'),
+            btndone = document.createElement('button')
+      btndone.innerHTML = 'Done'   
+      btndone.addEventListener('click', markDone)   
+      item.innerHTML = dodoText.value
+      item.appendChild(btndone)
+      dodoList.appendChild(item)
+      dodoText.value = ''   
+   }
+}
+
+function markDone(e) {
+   const dodo = e.path[1]   
+   dodo.parentNode.removeChild(dodo);
 }
